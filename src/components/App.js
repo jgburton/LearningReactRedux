732f253ec6f1137ad39def9aed8735667ab2c9fd
Jamie Grantham-Burton <jamie.granthamburton@zebra.com>
2020-09-09 11:40:19 +0100

    Set up redux reducers and actions

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { selectSong } from '../actions'; //named export - curly braces, default export no curly braces 
 
 
 const App = () => {
---
+++ src/components/SongList.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const SongList = () => {
+    return <div>SongList</div>
+}
+
+export default SongList;
