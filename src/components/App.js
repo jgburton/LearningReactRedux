3dda81c4ef53fbfaf8ea25e28d3e2d31e0df374c
Jamie Grantham-Burton <jamie.granthamburton@zebra.com>
2020-09-10 10:45:20 +0100

    render list

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { selectSong } from '../actions'; //named export - curly braces, default export no curly braces 
+import SongList from './SongList';
 
 
 const App = () => {
-    return <div>App</div>
-}
+    return (
+    <div className="ui container grid">
+        <div className="ui row">
+        <div className="column eight wide">
+            <SongList />
+        </div>
+        </div>
+        
+    </div>
+    );
+};
 
 export default App;
 
--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 
-const SongList = () => {
-    return <div>SongList</div>
-}
+class SongList extends React.Component {
+    renderList() {
+        return this.props.songs.map((song) => {
+            return (
+                <div className="item" key={song.title}>
+                    <div className="right floated content">
+                        <button className="ui button primary">Select</button>
+                    </div>
+                    <div className="content">{song.title}</div>
+                </div>
+            );
+        });
+    }
 
-export default SongList;
+    render() {
+        // console.log(this.props);
+        return <div className="ui divided list">{this.renderList()}</div>
+    }
+};
+
+const mapStateToProps = (state) => {
+
+    return { songs: state.songs };
+};
+
+export default connect(mapStateToProps)(SongList);
