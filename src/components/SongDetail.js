d92bca59dbbde451e0826fca8c3bec1ca690d056
Jamie Grantham-Burton <jamie.granthamburton@zebra.com>
2020-09-14 13:49:42 +0100

    hooking up the songDetail component with redux and react

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { connect } from "react-redux";
 
 
-const SongDetail = () => {
-    return <div>Song Detail</div>
+const SongDetail = ({ song }) => {
+    if (!song) {
+        return <div>Please Select a song...</div>
+    } 
+
+    return (
+    <div>
+        <h3>Details for:</h3>
+        <p>
+            Title: {song.title}
+            <br />
+            Duration: {song.duration}
+        </p>
+    </div>
+    )
 }
 
-export default SongDetail;
+const mapStateToProps = (state) => {
+    return { song: state.selectedSong };
+};
+
+export default connect(mapStateToProps)(SongDetail);
