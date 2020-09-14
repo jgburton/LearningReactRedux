65322c18697842d218d995143eb3291b07bbfd6f
Jamie Grantham-Burton <jamie.granthamburton@zebra.com>
2020-09-14 11:50:21 +0100

    Click on button to update state in redux store

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { selectSong } from '../actions';
+
 
 
 class SongList extends React.Component {
@@ -8,7 +10,7 @@ class SongList extends React.Component {
             return (
                 <div className="item" key={song.title}>
                     <div className="right floated content">
-                        <button className="ui button primary">Select</button>
+                        <button onClick={() => this.props.selectSong(song)} className="ui button primary">Select</button>
                     </div>
                     <div className="content">{song.title}</div>
                 </div>
@@ -23,8 +25,8 @@ class SongList extends React.Component {
 };
 
 const mapStateToProps = (state) => {
-
+    console.log(state);
     return { songs: state.songs };
 };
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, { selectSong })(SongList);
